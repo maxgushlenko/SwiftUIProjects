e80fe5e5647fa05cd2dc60b7fd1018e48277e1f8
Max Gushlenko <ueiktyrj@gmail.com>
2023-06-07 12:13:37 +0200

    WeSplit project was added.

---
+++ Projects/WeSplit/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by m4c5on on 07/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
