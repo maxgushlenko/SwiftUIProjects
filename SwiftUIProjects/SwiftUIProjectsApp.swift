c69c0838e9d3b99348be3f0bdd86ff3dbffb9a53
Max Gushlenko <ueiktyrj@gmail.com>
2023-06-07 11:55:54 +0200

    Initial Commit

---
+++ SwiftUIProjects/SwiftUIProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProjectsApp.swift
+//  SwiftUIProjects
+//
+//  Created by m4c5on on 07/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
